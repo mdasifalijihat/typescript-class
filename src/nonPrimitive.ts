6974f57d3475e3e7e3582921a427cf9e9fe74c4f
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-15 12:30:11 +0600

    added nonPrimitive object

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -17,3 +17,45 @@ let couple: [string, string] = ["asif", "wife"];
 let nameAndRoll: [string, number] = ["Asif", 79];
 
 let destination: [string, string, number] = ["Dhaka", "Chattogram", 3];
+
+// reference type : object
+
+// user name 3 word
+// const user: {
+//   firstName: string;
+//   middleName: string;
+//   lastName: string;
+// } = {
+//   firstName: "Asif",
+//   middleName: "Ali",
+//   lastName: "Jihat",
+// };
+
+// user name 2 word
+// const user: {
+//   firstName: string;
+//   middleName?: string; //optional type
+//   lastName: string;
+//   isMarried?: boolean;
+//   organization: "Programming Hero"; //value => type define : literal types
+// } = {
+//   firstName: "Tahirul",
+//   lastName: "Islam",
+//   isMarried: true,
+//   organization: "Programming Hero",
+// };
+
+const user: {
+  firstName: string;
+  middleName?: string; //optional type
+  lastName: string;
+  isMarried?: boolean;
+  readonly organization: string; //access modifier
+} = {
+  firstName: "Tahirul",
+  lastName: "Islam",
+  isMarried: true,
+  organization: "Programming Hero",
+};
+
+console.log(user);
