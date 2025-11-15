a208fad052d440979f5cba982151f2e4dba71b34
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-15 12:02:58 +0600

    added nonPrimitive type tuple

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,19 @@
+// array, object
+
+// ts-tuple
+
+let bazarList: string[] = ["eggs", "milk", "sugar"];
+
+bazarList.push("Mane");
+
+let mixedArr: (string | number)[] = ["eggs", 12, "milk", 1, "sugar", 2];
+
+mixedArr.push("apple", 12);
+
+let coordinates: [number, number] = [20, 30];
+
+let couple: [string, string] = ["asif", "wife"];
+
+let nameAndRoll: [string, number] = ["Asif", 79];
+
+let destination: [string, string, number] = ["Dhaka", "Chattogram", 3];
