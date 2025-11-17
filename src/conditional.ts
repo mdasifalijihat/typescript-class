f60fbd1f710e4a05ea42bf8d53944e3d56eae7cf
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-17 20:59:53 +0600

    added conditional type

---
+++ src/conditional.ts
@@ -0,0 +1,20 @@
+//conditional type : je type condition er upor nirvorsheel hoy
+
+type A = null;
+type B = undefined;
+
+type C = A extends number ? true : B extends undefined ? true : false;
+
+type RichPeoplesVehicle = {
+  bike: string;
+  car: string;
+  ship: string;
+};
+
+// type CheckVehicle<T> = T extends "bike" | "car" | "ship" ? true : false;   //not use
+
+type CheckVehicle <T> = T extends keyof RichPeoplesVehicle ? true : false; //use this
+
+type HasBike = CheckVehicle<"ship">;
+
+
