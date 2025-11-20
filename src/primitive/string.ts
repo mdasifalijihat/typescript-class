0187abead38b23e59768578e97d00078b65faae2
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-20 23:10:52 +0600

    added stirng

--- src/primitive/string.ts
+++ src/primitive/string.ts
@@ -1,11 +1,41 @@
-let name: string = 'asif';
+let name: string = "asif";
 
 let sms: string = `Hello ${name}, Welcome`;
-console.log(sms);
+// console.log(sms);
 
-let home: string = 'Dhaka';
+let home: string = "Dhaka";
 let phoneNo: string = "017166-733245";
 let love: string = "I don`t love this";
 
-let print: string = `'Home: ${home}, Phone No: ${phoneNo}, Love: ${love}'`
-console.log(print)
+let print: string = `'Home: ${home}, Phone No: ${phoneNo}, Love: ${love}'`;
+// console.log(print)
+
+let firstName: string = "Asif";
+let lastName: string = "jiht";
+let fullName: string = firstName + " " + lastName;
+// console.log(fullName)
+
+let lengthOfName = fullName.length;
+// console.log(lengthOfName)
+
+// upper case
+let upperCase = fullName.toUpperCase();
+// console.log(upperCase)
+
+// lower case
+let loweCase = fullName.toLowerCase();
+// console.log(loweCase)
+
+// find
+
+let findName = fullName.includes("Asif");
+// console.log(findName);
+
+let email: string = "asifAliLi@gmail.com";
+let phoneEm: string = "014711544874";
+
+let printEmail = email.toLowerCase();
+// console.log(printEmail)
+
+let addFull: string = `Email: ${printEmail}, Phone No: ${phoneEm}`;
+console.log(addFull);
